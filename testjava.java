f30245bd51304dc97755f52d198dff2bc3290d63
spartxn-18 <pravinrstn92@gmail.com>
2025-02-08 16:19:45 +0530

    Update testjava.java

--- testjava.java
+++ testjava.java
@@ -1,7 +1,4 @@
 public static void main(String args[])
 {
 System.out.println("Hello World");
-System.out.println("Priyadharshini");
-System.out.println("feature added");
-  System.out.println("Hello World");
 }
